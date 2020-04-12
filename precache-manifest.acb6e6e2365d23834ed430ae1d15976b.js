8bdecb987677e9432a16e2bbcb645a0231b4c72d
Abhinandan <abhi.kk747@gmail.com>
2020-04-13 02:01:07 +0530

    Updates

---
+++ precache-manifest.acb6e6e2365d23834ed430ae1d15976b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c421eda57daf6eed4dee63aff1a7baf5",
+    "url": "/react-furniture-shop/index.html"
+  },
+  {
+    "revision": "7e961af6ab8970e572cc",
+    "url": "/react-furniture-shop/static/css/2.dfee3dab.chunk.css"
+  },
+  {
+    "revision": "b8ffc82caa96e8259062",
+    "url": "/react-furniture-shop/static/css/main.404276f1.chunk.css"
+  },
+  {
+    "revision": "7e961af6ab8970e572cc",
+    "url": "/react-furniture-shop/static/js/2.2c894c9e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-furniture-shop/static/js/2.2c894c9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ffc82caa96e8259062",
+    "url": "/react-furniture-shop/static/js/main.93dde723.chunk.js"
+  },
+  {
+    "revision": "2b49e630f42961011a33",
+    "url": "/react-furniture-shop/static/js/runtime-main.31099c09.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-furniture-shop/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-furniture-shop/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-furniture-shop/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-furniture-shop/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-furniture-shop/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "da59576b18073bb6cfd6fc347fddaf19",
+    "url": "/react-furniture-shop/static/media/item6.da59576b.jpg"
+  },
+  {
+    "revision": "323ea1c75215431446b70951a6c6eec5",
+    "url": "/react-furniture-shop/static/media/sofa.323ea1c7.jpeg"
+  }
+]);
